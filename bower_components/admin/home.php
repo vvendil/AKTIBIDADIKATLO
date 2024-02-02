3609a26e8566f414cd92f8d053cdf8fcba2143ee
CJSigua <144505229+CJSigua@users.noreply.github.com>
2024-02-02 18:26:21 +0800

    Add files via upload

---
+++ bower_components/admin/home.php
@@ -0,0 +1,39 @@
+<?php 
+  include 'includes/session.php';
+  include 'includes/format.php'; 
+?>
+<?php 
+  $today = date('Y-m-d');
+  $year = date('Y');
+  if(isset($_GET['year'])){
+    $year = $_GET['year'];
+  }
+
+  $conn = $pdo->open();
+?>
+<?php include 'includes/header.php'; ?>
+<body class="hold-transition skin-blue sidebar-mini">
+<div class="wrapper">
+
+  <?php include 'includes/navbar.php'; ?>
+  <?php include 'includes/menubar.php'; ?>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Dashboard
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li class="active">Dashboard</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      
+      
+</body>
+</html>

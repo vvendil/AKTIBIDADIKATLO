ff1f6e5a2a631d48536fa9a00afc0c540d7129f7
vvendil <avendil733@gmail.com>
2024-02-02 11:19:54 +0800

    added space

--- index.php
+++ index.php
@@ -16,6 +16,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
             </div>
 
+            
         </div> 
         <div class="content">
         <div class="slideshow-container">
